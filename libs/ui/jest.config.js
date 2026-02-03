1c7b623cc9000fdf7b942ce3b1f69bf4d4e9d6ab
AlexyWCK <you@example.com>
2026-02-03 16:52:34 +0100

    tests

--- libs/ui/jest.config.js
+++ libs/ui/jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "jsdom",
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
   },
